a07c37f5c969f6dce481097eb26e3b66b32c3709
Mike Agoya <agoyamike@gmail.com>
2025-12-13 09:46:31 +0300

    feat: Initialize users and products apps with basic models and admin integration

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+class Product(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
