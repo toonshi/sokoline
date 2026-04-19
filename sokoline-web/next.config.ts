4bad18422ef44ff0459f5a4a249c798365665681
Mike Agoya <agoyamike@gmail.com>
2026-04-19 12:17:17 +0300

    chore: Decouple sub-repositories from root and update .gitignore

--- sokoline-web/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ["192.168.100.17", "localhost:3000"],
-};
-
-export default nextConfig;
--- sokoline-web/types/vitest-tsconfig-paths.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'vitest-tsconfig-paths' {
-  import type { PluginOption } from 'vite';
-
-  export default function tsconfigPaths(): PluginOption;
-}
